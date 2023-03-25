6966750f351db2e7aec0982d5f0e116011774974
Vera Maria <mymadonna89@mail.ru>
2023-03-25 19:25:33 +0600

    'task_solution' solved

--- task_solution/Program.cs
+++ task_solution/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] filledArray = new string[7] {"git", "java", "C#", "python", "github", "sql", "GB"};
+string[] currentArray = new string[7];
+
+void NewArray(string[] filledArray, string[] currentArray)
+{
+    int count = 0;
+    for (int i = 0; i < filledArray.Length; i++)
+    {
+    if(filledArray[i].Length <= 3)
+        {
+        currentArray[count] = filledArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray(filledArray, currentArray);
+PrintArray(currentArray);
